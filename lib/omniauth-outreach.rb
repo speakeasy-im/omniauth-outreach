e8fffc2d28ee08cd2a72fb35e2f4b47dda40e359
Kash Nouroozi <knouroozi@gmail.com>
2020-06-11 13:01:15 -0400

    Update omniauth-outreach.rb

--- lib/omniauth-outreach.rb
+++ lib/omniauth-outreach.rb
@@ -1,2 +1,2 @@
-require_relative "omniauth-outreach/version" # rubocop:disable FileName
+require_relative "omniauth-outreach/outreach/version" # rubocop:disable FileName
 require_relative "omniauth/strategies/outreach"
